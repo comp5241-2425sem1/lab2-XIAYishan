919a5675bcdbf693b1a40a0416153860d0b3533c
XIAYishan <24045097g@connect.polyu.hk>
2024-09-26 05:26:30 +0000

    Completed task 2

---
+++ app2.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import llm
+
+# Create a text input box
+text_input = st.text_input("Input your question")
+
+# Create a submit button
+if st.button("Submit"):
+    user_prompt = text_input
+    system_prompt = "Answer questions in a funny tone with Emoji"
+    result = llm.answer(system_prompt, user_prompt)
+    st.markdown("## Response")
+    st.write("Response:", result)
